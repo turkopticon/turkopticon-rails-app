f11cdb66ac19cc9c21e7bff1ddc751a90f99b6a7
Six Silberman <six@wtf.tw>
2015-03-21 09:23:02 -0700

    Create rating/review stats page, add link to new page to main stats page

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -14,4 +14,18 @@ class StatsController < ApplicationController
     @posting_users = reps.map{|r| r.person_id}.uniq.count
   end
 
+  def reviews
+    reports = Report.all
+    @scores = {}
+    attrs = %w{comm pay fair fast}
+    attrs.each{|attr| @scores[attr] = {} }
+    [1, 2, 3, 4, 5].each{|i| attrs.each{|attr| @scores[attr][i] = 0}}
+    reports.each{|r|
+      attrs.each{|attr|
+        attr_val = eval("r." + attr)
+        @scores[attr][attr_val] += 1 unless attr_val.nil? or attr_val == 0
+      }
+    }
+  end
+
 end
