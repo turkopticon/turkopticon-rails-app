e30b4fe4f9467a7012f2f03ab15b0360437bee36
Kai Li <ykzihel@gmail.com>
2016-10-27 08:01:33 -0400

    refactor: reorganize root pages, remove old global layout

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,10 +1,11 @@
 class MainController < ApplicationController
 
-  before_filter :authorize, :except => [:requester_stats, :info, :help, :help_v2, :install_v2, :install_welcome, :requester_attrs, :requester_attrs_v2, :ditz, :blog, :post, :blogfeed, :requesters, :requester_attrs_2, :x, :ferret_index, :rules, :dedupe_reqs, :backup_db, :check_for_duplicate_requester_objects, :wth]
-  before_filter :check_for_existing_report, :only => :add_report
-  before_filter :verify, :only => :add_report
-  before_filter :authorize_as_commenter, :only => [:add_comment, :add_flag]
+  # before_filter :authorize, :except => [:requester_stats, :info, :help, :help_v2, :install_v2, :install_welcome, :requester_attrs, :requester_attrs_v2, :ditz, :blog, :post, :blogfeed, :requesters, :requester_attrs_2, :x, :ferret_index, :rules, :dedupe_reqs, :backup_db, :check_for_duplicate_requester_objects, :wth]
+  # before_filter :check_for_existing_report, :only => :add_report
+  # before_filter :verify, :only => :add_report
+  # before_filter :authorize_as_commenter, :only => [:add_comment, :add_flag]
 
+=begin
   def pri
   end
 
@@ -459,14 +460,6 @@ class MainController < ApplicationController
     end
   end
 
-  def info
-    @location = "about"
-  end
-
-  def info2
-    @location = "about"
-  end
-
   def install_welcome
     @location = "install_welcome"
   end
@@ -593,5 +586,5 @@ class MainController < ApplicationController
       render :text => "Duplicate Requester objects exist for requesters with IDs " + duplicate_ids.join(", ")
     end
   end
-
+=end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   get 'splash/index' # only used for initial testing
 
   root 'main#info'
+  get 'install' => 'main#install'
+  get 'rules' => 'main#rules'
+  resources 'reviews'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
@@ -10,22 +13,15 @@ end
 
 =begin
 ActionController::Routing::Routes.draw do |map|
-  map.root :controller => "main", :action => "info"
   map.connect '2016survey', :controller => "main", :action => "survey2016"
   map.connect 'forum', :controller => "forum", :action => "index"
-  map.connect 'rules', :controller => "main", :action => "rules"
   map.connect 'stats', :controller => "stats"
   map.connect 'mod', :controller => "mod"
-  map.connect 'x', :controller => "main", :action => "x"
   map.connect 'pri', :controller => "main", :action => "pri"
   map.connect 'issues', :controller => "main", :action => "issues"
   map.connect 'login', :controller => "reg", :action => "login"
   map.connect 'logout', :controller => "reg", :action => "logout"
   map.connect 'register', :controller => "reg", :action => "register"
-  map.connect 'report', :controller => "main", :action => "add_report"
-  map.connect 'edit/:id', :controller => "main", :action => "edit_report"
-  map.connect 'reports', :controller => "main", :action => "index"
-  map.connect 'edit/:id', :controller => "main", :action => "edit_report"
   map.connect 'info', :controller => "main", :action => "info"
   map.connect 'ditz', :controller => "main", :action => "ditz"
   map.connect 'blog', :controller => "main", :action => "blog"
@@ -39,19 +35,10 @@ ActionController::Routing::Routes.draw do |map|
   map.connect 'all_requesters', :controller => "main", :action => "all_requesters"
   map.connect 'requesters.csv', :controller => "main", :action => "requesters", :format => "csv"
   map.connect 'settings', :controller => "reg", :action => "settings"
-  map.connect 'my_flagged', :controller => "main", :action => "my_flagged"
-  map.connect 'flagged', :controller => "main", :action => "flagged"
   map.connect 'hidden', :controller => "main", :action => "hidden"
-  map.connect 'my_reviews', :controller => "main", :action => "my_reviews"
-  map.connect 'by/:id', :controller => "main", :action => "reports_by"
-  map.connect 'flagged_by/:id', :controller => "main", :action => "flagged_by"
-  map.connect 'comments_by/:id', :controller => "main", :action => "comments_by"
-  map.connect 'all_by/:id', :controller => "main", :action => "all_by"
   map.connect 'reviews_by_one_page/:id', :controller => "main", :action => "reports_by_one_page"
   map.connect 'admin', :controller => "admin", :action => "index"
   map.connect 'wth', :controller => "main", :action => "wth"
-  map.connect ':id', :controller => "main", :action => "index"
-  map.connect 'get_report/:id', :controller => "main", :action => "report"
   map.connect 'requester_stats/:id', :controller => "main", :action => "requester_stats"
   map.connect 'attrs/:id', :controller => "main", :action => "requester_attrs"
   map.connect 'attrsv2/:id', :controller => "main", :action => "requester_attrs_v2"
