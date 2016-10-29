55951cde44fff1b56a67216363e5bd836c29c209
Kai Li <ykzihel@gmail.com>
2016-10-29 03:47:20 -0400

    refactor: extract bold font weight to a general rule

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   get 'install' => 'main#install'
   get 'rules' => 'main#rules'
   resources 'reviews'
+  get 'requesters' => 'requesters#index'
+  get 'requesters/:id' => 'requesters#show'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
