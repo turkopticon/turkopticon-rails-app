39d31a690bc45dabdb252f349b19c0d2082c6186
Kai Li <ykzihel@gmail.com>
2016-11-03 03:43:13 -0400

    feat(controllers): interface with models

--- app/controllers/requesters_controller.rb
+++ app/controllers/requesters_controller.rb
@@ -2,4 +2,9 @@ class RequestersController < ApplicationController
   def index
     @page = { title: params[:s] ? "Search results for #{params[:s]}" : 'Recently reviewed requesters' }
   end
+
+  def show
+    req   = Requester.by_rid(params[:rid])
+    @page = { requester: req, reviews: req.reviews.order(created_at: :desc), aggregates: req.aggregates }
+  end
 end
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,10 +1,13 @@
 class ReviewsController < ApplicationController
 
   def index
-    query = params.extract!(:user, :comments, :flags)
+    # TODO: fully integrate queries
+    query = params.slice(:user, :comments, :flags)
     @page = { location: query.values.reject { |v| v == 'false' }.length > 0 ? 'Reviews' : 'Recent Reviews',
-              rname:    params[:rid] && "for #{params[:rid]}",
-              params:   query.to_unsafe_h }
+              rname:    params[:rid] && "for #{Requester.find_by(params[:rid]).rname || params[:rid]}",
+              params:   query.to_unsafe_h,
+              reviews:  query[:user] ? Review.by_user(query[:user]) : Review.newest(25) }
+
   end
 
 end
