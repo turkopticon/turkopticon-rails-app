5209bb05c5e6536f2171986c1e61d1058f090603
Kai Li <ykzihel@gmail.com>
2016-11-03 03:09:07 -0400

    feat(helpers): add pay rate and time conversions

--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -1,7 +1,7 @@
 module ReviewsHelper
 
   def semantic_params(params)
-    params[:comments] = true if params[:user].present?
+    # params[:comments] = true if params[:user].present?
     params.delete_if { |_, v| v == 'false' }
 
     sem = params.sort.map do |v|
@@ -13,12 +13,44 @@ module ReviewsHelper
         when :flags, 'flags' then
           :flags
         else
-          ''
+          nil
       end
     end
 
     sem.insert(1, :and) if params.key?(:flags) && params.key?(:comments)
     sem.insert(0, :with) unless sem.length < 1
     sem.join ' '
+  end
+
+  def pay_rate(pay, time, opt = {})
+    opt = { unit: :hr, split: false }.merge(opt)
+    return nil unless time && time > 0
+    base = pay/time
+    exp  = case opt[:unit]
+             when :hr, :h, :hour then
+               2
+             when :min, :m, :minute then
+               1
+             else
+               0
+           end
+    str  = '$%.2f' % (base * 60 ** exp)
+    return str << "/#{opt[:unit]}" unless opt[:split]
+    [str, opt[:unit]]
+  end
+
+  def htime(sec, approx = true)
+    sec = sec.to_i
+    return nil unless sec && sec > 0
+    if sec > 86400 && approx
+      '%.2f days' % (sec/86400.0)
+    else
+      units = %w(d h m s)
+      [sec/86400, sec%86400/3600, sec/60%60, sec%60]
+          .map { |v| v.to_s.rjust(2, '0') }
+          .map.with_index { |v, i| v << units[i] }
+          .select { |v| v =~ /[1-9]/ }
+          .join
+    end
   end
 end
