f4ee12592832ab5e0be3832c397d2f6fab391d57
Kai Li <ykzihel@gmail.com>
2016-11-03 23:27:18 -0400

    feat(js/reviews): add 'length between' validator

--- app/assets/javascripts/reviews/_validator.js
+++ app/assets/javascripts/reviews/_validator.js
@@ -3,42 +3,40 @@
       required         : _required,
       pattern          : _pattern,
       lengthGreaterThan: _lenGT,
+      lengthBetween    : _lenIn,
       valueGreaterThan : _valGT,
       negPattern       : _negPattern
     };
 
   function _required(value) {
-    return value !== null && value.toString().length > 0 ? [true, null] : [false, 'Required'];
+    return value !== null && value !== undefined && value.toString().length > 0 ? [true, null] : [false, 'Required'];
   }
 
-  //function _length(value, range) {
-  //  if (range.length === 1)
-  //    return value.length === range[0]
-  //      ? [true, null] : [false, `Length must be ${range[0]}`];
-  //  else
-  //    return value.length >= range[0] && value.length <= range[1]
-  //      ? [true, null] : [false, `Length must be at least ${range[0]}`]
-  //}
-
   function _valGT(value, min, name, units) {
-    return value > min
+    return value && value > min
       ? [true, null]
       : [false, `${name || 'Value'} must be greater than ${min} ${units || ''}`];
   }
 
   function _lenGT(value, min) {
-    return value.length > min ? [true, null] : [false, `Length must be greater than ${min}`];
+    return value && value.length > min ? [true, null] : [false, `Length must be greater than ${min}`];
+  }
+
+  function _lenIn(value, min, max) {
+    return value && value.length <= max && value.length >= min
+      ? [true, null]
+      : [false, `Length (${value.length}) must be between ${min} and ${max}`]
   }
 
   function _lenLT(value, max) {
-    return value.length > max ? [true, null] : [false, `Length must be less than ${max}`];
+    return value && value.length > max ? [true, null] : [false, `Length must be less than ${max}`];
   }
 
   function _negPattern(value, re, name) {
-    return re.test(value) ? [false, `Improperly formatted ${name || 'value'}`] : [true, null];
+    return value && re.test(value) ? [false, `Improperly formatted ${name || 'value'}`] : [true, null];
   }
 
   function _pattern(value, re, name) {
-    return re.test(value) ? [true, null] : [false, `Improperly formatted ${name || 'value'}`];
+    return value && re.test(value) ? [true, null] : [false, `Improperly formatted ${name || 'value'}`];
   }
 })(window._rf);
