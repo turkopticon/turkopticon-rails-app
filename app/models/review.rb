e0fb74bbd12199d9b45b2d4073646c32e124a610
Kai Li <ykzihel@gmail.com>
2016-11-04 05:36:17 -0400

    feat(models/review): ensure parents exist before validation

--- app/models/review.rb
+++ app/models/review.rb
@@ -4,6 +4,8 @@ class Review < ApplicationRecord
   has_one :requester, through: :hit
   has_many :comments
 
+  attr_writer :dependent_params
+
   default_scope { includes(:requester, :hit, :person) }
   scope :newest, -> (max) { order(created_at: :desc).limit(max) }
   scope :of_requester, -> (rid) { where('requesters.rid': rid) }
@@ -12,12 +14,27 @@ class Review < ApplicationRecord
   scope :recent, -> (frame = (90.days.ago .. Time.now)) { where(created_at: frame) }
   # scope :with, -> (*args) { joins(*args) }
 
+  before_validation :dont_orphan_me_bro
+
   def tags
     tags = %w( tos broken deceptive recommend )
                .map { |t| [t, self[t], self["#{t}_context"]] }
                .select { |t| t[1] == true || t[1] =~ '(no|yes)' }
     tags.unshift ['rejected', true, 'some or all of my work was rejected'] if self.rejected == 'yes'
     tags
+  end
+
+  private
+
+  def dont_orphan_me_bro
+    params = @dependent_params || {}
+    person = Person.find(params[:user])
+    req    = Requester.find_or_initialize_by(rid: params[:rid]).manage_alias(params[:rname])
+    req.save
+    hit            = Hit.find_or_create_by!(title: params[:title], reward: params[:reward], requester_id: req.id)
+    self.person    = person
+    self.requester = req
+    self.hit       = hit
   end
 
 end
