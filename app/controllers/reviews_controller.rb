df8da8999e5fa12c32d49cf84afdae2e317724b2
Kai Li <ykzihel@gmail.com>
2016-11-06 06:27:15 -0500

    feat(controllers/reviews): add actions for creating and editing reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -10,4 +10,47 @@ class ReviewsController < ApplicationController
 
   end
 
+  def new
+    @review = Review.new
+    @state  = params.slice(:rid, :rname, :title, :reward).permit!.to_json
+  end
+
+  def edit
+    @review = Review.find(params[:id])
+    rev     = @review.as_json.reject { |k, v| k =~ /(_(?:id|at|.+ew)\z|\Aid)/ || v.nil? }
+    hit     = @review.hit.as_json.select { |k| %w(title reward).include? k }
+    req     = @review.requester.as_json.select { |k| %w(rname rid).include? k }
+    @state  = rev.merge(hit.merge req).to_json
+  end
+
+  def create
+    review_params            = form_params.reject { |k| %w(rid rname title reward).include? k }
+    @review                  = Review.new(review_params)
+    @review.dependent_params = form_params.select { |k| %w(rid rname title reward).include? k }.merge user: @user
+
+    if @review.save
+      redirect_to requester_path @review.requester.rid
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    review_params            = form_params.reject { |k| %w(rid rname title reward).include? k }
+    @review                  = Review.find(params[:id])
+    @review.dependent_params = form_params.select { |k| %w(rid rname title reward).include? k }.merge user: @user
+
+    if @review.update(review_params)
+      redirect_to requester_path @review.requester.rid
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+  def form_params
+    JSON.parse(params.require(:review).permit(:state).to_h[:state])
+  end
+
 end
