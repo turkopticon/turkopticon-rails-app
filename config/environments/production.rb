333003fc60d92f137e67f3c9f244c13c5b2105c9
Kai Li <ykzihel@gmail.com>
2016-11-27 08:50:16 -0500

    chore: set production to use memcache

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,18 +44,18 @@ Rails.application.configure do
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
-  config.log_level                         = :debug
+  config.log_level                     = :debug
 
   # Prepend all log lines with the following tags.
-  config.log_tags                          = [:request_id]
+  config.log_tags                      = [:request_id]
 
   # Use a different cache store in production.
-  # config.cache_store = :mem_cache_store
+  config.cache_store                   = :mem_cache_store
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "default-mysql_#{Rails.env}"
-  config.action_mailer.perform_caching     = false
+  config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
