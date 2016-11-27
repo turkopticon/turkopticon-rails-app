21138a48f4bcf04623043cc0041d9977f767c5a3
Kai Li <ykzihel@gmail.com>
2016-11-27 07:27:55 -0500

    perf(api): use caching

--- app/controllers/api/v2d0a/requesters_controller.rb
+++ app/controllers/api/v2d0a/requesters_controller.rb
@@ -12,22 +12,22 @@ class Api::V2d0a::RequestersController < Api::V2d0a::ApiController
     success, attrs = field_params
     return render json: attrs, status: :bad_request unless success
 
-    requesters = rids.map { |rid| Requester.find_by(rid: rid) }.select { |r| r }
-                     .map { |r| r.as_json.merge({ 'aggregates' => r.aggregates }) }
+    requesters = Requester.cached_multi_find_by(rid: rids).select { |_, v| v }
+                     .map { |_, v| v.as_json.merge({ 'aggregates' => v.cached_aggregates }) }
 
     status, json = ::RequestersSerializer.new(requesters, collection: true, attrs: attrs).call
     render json: json, status: status
   end
 
   def show
-    requester = Requester.find_by(rid: params[:rid])
+    requester = Requester.cached_find_by(rid: params[:rid])
     unless requester
       msg = "RequesterId '#{params[:rid]}' does not exist in the database"
       err = Api::ApiErrorObject.new(:not_found, message: msg).call
       return render json: err, status: :not_found
     end
 
-    requester      = requester.as_json.merge({ 'aggregates' => requester.aggregates })
+    requester      = requester.as_json.merge({ 'aggregates' => requester.cached_aggregates })
     success, attrs = field_params
     return render json: attrs, status: :bad_request unless success
 
