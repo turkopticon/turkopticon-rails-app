d773715e381a532cd271a7a2044f1602551c1196
Kai Li <ykzihel@gmail.com>
2017-02-05 00:46:25 -0500

    fix(controllers/requesters): incorrect scoping

--- app/controllers/requesters_controller.rb
+++ app/controllers/requesters_controller.rb
@@ -4,16 +4,16 @@ class RequestersController < ApplicationController
   def index
     @page              = { title: params[:q] ? "Search results for '#{params[:q]}'" : 'Recently reviewed requesters' }
     @page[:requesters] = if params[:q].present?
-                           Requester.name_search(params[:q]).page(params[:page] || 1)
+                           Requester.name_search(params[:q]).page(params[:page])
                          else
-                           Requester.page(params[:page] || 1)
+                           Requester.page(params[:page])
                          end
   end
 
   def show
     req                = Requester.cached_find_by(rid: params[:rid])
     @page              = { requester: req }
-    @page[:reviews]    = req.reviews.newest.page(params[:page] || 1)
+    @page[:reviews]    = req.reviews.newest.valid.page(params[:page])
     @page[:aggregates] = req.cached_aggregates
   end
 end
