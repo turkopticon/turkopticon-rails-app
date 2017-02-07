c13b87847947222f89948db4a15fa90254502682
Kai Li <ykzihel@gmail.com>
2017-02-07 05:56:37 -0500

    feat(views/requesters): add AMT search and contact links

---
+++ app/helpers/requesters_helper.rb
@@ -0,0 +1,20 @@
+module RequestersHelper
+
+
+  def amt_search(requester)
+    build_uri 'searchbar', selectedSearchType: 'hitgroups', requesterId: requester.rid
+  end
+
+  def amt_contact(requester)
+    build_uri 'contact', requesterId: requester.rid, requesterName: requester.rname
+  end
+
+  protected
+
+  def build_uri(path, query={})
+    amt_base  = 'https://www.mturk.com/mturk/'.freeze
+    uri       = URI.join amt_base, path
+    uri.query = query.to_query
+    uri.to_s
+  end
+end
