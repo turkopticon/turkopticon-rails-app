2c12884e03ef413de245c8936b1c833859f8c19a
Kai Li <ykzihel@gmail.com>
2017-02-17 06:53:30 -0500

    fix: haml renderer indenting multiline text inputs
    Apparently only affects development environment, but
    add it in anyways.

---
+++ config/initializers/haml.rb
@@ -0,0 +1,5 @@
+# whitespace indent bug (apparently development only)
+# see: https://github.com/haml/haml/issues/643
+# also: http://haml.info/docs/yardoc/file.FAQ.html
+
+Haml::Template.options[:ugly] = true
